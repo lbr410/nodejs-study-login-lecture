f6e0e6057a9dfc8e40ffd6290a85abfafecd1759
lbr <lbor3280@naver.com>
2024-06-08 05:34:20 +0900

    DOM 이용하여 HTML 객체 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,14 @@
 "use strict";
 
-console.log("hello");
-console.log("bye");
+const id = document.querySelector("#id"), // tag에 부여된 id
+    password = document.querySelector("#password"),
+    loginBtn = document.querySelector("button"); // tag 그대로 불러옴
+
+loginBtn.addEventListener("click", login);
+
+function login() {
+    const req = {
+        id: id.value,
+        password: password.value,
+    };
+}
