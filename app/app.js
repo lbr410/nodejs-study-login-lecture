ee92c9037c33b3d648e58ea3d7ff8ce8735bd2ae
lbr <lbor3280@naver.com>
2024-06-08 04:50:55 +0900

    프론트 단 js 파일 만들기

--- app/app.js
+++ app/app.js
@@ -12,6 +12,7 @@ app.set("views", "./src/views"); // views를 setting
 app.set("views engine", "ejs"); // ejs는 html과 비슷한 엔진
 
 // middleware
+app.use(express.static(`${__dirname}/src/public`)); // 정적 경로 추가, __dirname은 app.js의 현재경로
 app.use("/", home); // use => 미들웨어를 등록해주는 메서드 / 루트 경로로 오면 home 경로로 이동
 
 module.exports = app;
