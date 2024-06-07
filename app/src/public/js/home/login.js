83d2552ab311b3242f7504b8d5ea25dee0e7570d
lbr <lbor3280@naver.com>
2024-06-08 05:53:59 +0900

    로그인을 요청하는 fetch 생성

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -7,8 +7,19 @@ const id = document.querySelector("#id"), // tag에 부여된 id
 loginBtn.addEventListener("click", login);
 
 function login() {
-    const req = {
+    const req = { // object(객체) 형태
         id: id.value,
         password: password.value,
     };
+
+    // console.log(req, JSON.stringify(req));
+    // console.log(JSON.stringify(req));
+
+    fetch("/login", { // object(객체) 형태
+        method: "POST",
+        headers: {
+            "Content-Type":"application/json" // 내가 보내는 데이터의 타입을 명시
+        },
+        body: JSON.stringify(req), // json 데이터 타입으로 데이터 전달
+    });
 }
