32bf13505bf82e0a776b0ce2cc6dbd253cab1b52
lbr <lbor3280@naver.com>
2024-06-09 09:25:10 +0900

    USer 모델 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -20,6 +20,18 @@ class UserStorage {
         }, {})
         return newUsers;
     };
+
+    static getUserInfo(id) {
+        const users = this.#users;
+        const idx = users.id.indexOf(id);
+        const usersKeys = Object.keys(users); // users의 키값들만 리스트로 만듦 => [id, password, name]
+        const userInfo = usersKeys.reduce((newUser, info) => {
+            newUser[info] = users[info][idx];
+            return newUser;
+        }, {})
+
+        return userInfo;
+    }
 }
 
 module.exports = UserStorage;
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,6 +1,7 @@
 "use strict";
 
-const UserStorage = require("../../models/UserStorage");
+const User = require("../../models/User");
+// const UserStorage = require("../../models/UserStorage");
 
 const output = {
     home: (req, res) => {
@@ -13,29 +14,33 @@ const output = {
 
 const process = {
     login: (req, res) => {
+        const user = new User(req.body);
+        const response = user.login();
+        return res.json(response); // json 객체로 만들어 클라이언트에게 던져줌
+
         // console.log(req.body); // 프론트에서 전달한 body 데이터
         // console.log(res);
 
-        const id = req.body.id,
-            password = req.body.password;
+        // const id = req.body.id,
+        //     password = req.body.password;
 
         // const userStorage = new UserStorage(); // 클래스니까 인스턴스 만들 때 이렇게 만듦
         // console.log(UserStorage.getUsers("id", "password", "name"));
         // id와 password만 반환하게 해주려면 이렇게 받아옴. 이 두 개의 필드만 받아와줌
-        const users = UserStorage.getUsers("id", "password");
+        // const users = UserStorage.getUsers("id", "password");
         
-        const response = {};
-        if(users.id.includes(id)) { // 프론트엔드에서 전달한 id가 users의 id에 있으면
-            const idx = users.id.indexOf(id);
-            if(users.password[idx] === password) {
-                response.success = true;
-                return res.json(response);
-            }
-        }
+        // const response = {};
+        // if(users.id.includes(id)) { // 프론트엔드에서 전달한 id가 users의 id에 있으면
+        //     const idx = users.id.indexOf(id);
+        //     if(users.password[idx] === password) {
+        //         response.success = true;
+        //         return res.json(response);
+        //     }
+        // }
 
-        response.success = false;
-        response.msg = "로그인이 실패하였습니다.";
-        return res.json(response);
+        // response.success = false;
+        // response.msg = "로그인이 실패하였습니다.";
+        // return res.json(response);
     },
 };
 
